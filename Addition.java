b9700e3c7fe579988c951eb77f0812881388fb02
kmsiddesh <127389350+kmsiddesh@users.noreply.github.com>
2023-03-09 14:48:17 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Addition
+{
+ public static void main(String[] args)
+ {
+  int num1=10,num2=20,sum=0;
+  
+  system.out.println("num1 = ' + num1);
+  system.out.println("num2 = ' + num2);
+  
+  sum = num1 + num2;
+  System.out.println("The sum = ' +sum);
+ }
+} 
